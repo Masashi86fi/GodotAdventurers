3328fb1aea95d2528b564283ddbb12b6e3f08e2d
Matti Anttila <masashiro86@gmail.com>
2020-03-07 22:30:11 +0200

    Ledge Grab Added
    Ledge grab added, Use action button "Use" 'e' key to hold on the ledge, NOTE: Decide better key mapping for ledge grab, but it works for now, also needs animations for all actions

--- src/Actors/PlayerCharacter/Scripts/PlatformMovement2.gd
+++ src/Actors/PlayerCharacter/Scripts/PlatformMovement2.gd
@@ -11,6 +11,7 @@ var canClimb = false
 var canJump = false
 var canRope = false
 var hLadder = false
+var onLedge = false
 var movement = Vector2()
 var friction = false	
 export var modGravity = 0.0
@@ -25,6 +26,8 @@ var inputAction
 onready var mySprite = $Sprite	
 onready var combo_system = $ComboControl
 onready var input_commands = $InputCommands
+onready var ledge_ray_up = $LedgeRays/LedgeRayUp
+onready var ledge_ray_down = $LedgeRays/LedgeRayDown
 
 func _ready() -> void:
 	state_machine = $AnimationTree.get("parameters/playback")
@@ -38,10 +41,10 @@ func _physics_process(delta):
 		movement.x = lerp(movement.x,0,groundFriction)
 		
 	else:
-		if not isClimbing:
+		if not isClimbing or not onLedge:
 			movement.x = lerp(movement.x,0,airResistance)
 			modGravity = GRAVITY
-			canJump = false
+			canJump = false	
 	
 	move()
 	readActionButtons()
@@ -65,14 +68,14 @@ func readActionButtons():
 
 func move():
 	readAxis()
-	var currentState= state_machine.get_current_node()	
-			
+	var currentState= state_machine.get_current_node()		
 	if input_axis.x > 0:
 		#movement.x += input_axis.x * ACCELERATION
 		movement.x = min(movement.x + (input_axis.x * ACCELERATION), MAXSPEED)
 		mySprite.flip_h = false		
 		if sign($WeaponControl.scale.x) == -1:
 			$WeaponControl.scale.x *=-1
+			$LedgeRays.scale.x *=-1
 	
 	if input_axis.x < 0:
 		#movement.x += input_axis.x * ACCELERATION
@@ -80,6 +83,7 @@ func move():
 		mySprite.flip_h = true	
 		if sign($WeaponControl.scale.x) == 1:
 			$WeaponControl.scale.x *= -1
+			$LedgeRays.scale.x *=-1
 	
 	if input_axis.abs().x > 0.1 and is_on_floor():
 		runAnim()	
@@ -95,6 +99,10 @@ func move():
 		
 	if canRope:
 		rope(input_axis)
+	
+	ledgeGrab()	
+	
+	
 		
 func ladder(inputs):
 	
@@ -111,7 +119,8 @@ func ladder(inputs):
 		movement.x = lerp(movement.normalized().x,0,1)
 		movement.y = lerp(movement.y,0,groundFriction)
 		canJump = true
-		state_machine.travel("Idle")
+		state_machine.travel("Idle")	
+	
 	pass
 
 func rope(inputs):
@@ -139,8 +148,20 @@ func jump():
 		
 		if hLadder:
 			hLadder = false		
+		
+		if onLedge:
+			onLedge = false
 	pass
-
+	
+func ledgeGrab():
+	onLedge = (ledge_ray_down.is_colliding() and ledge_ray_up.is_colliding() == false)
+	
+	if onLedge and Input.is_action_pressed("Use"):
+		modGravity = 0
+		movement.x = lerp(movement.normalized().x,0,1)
+		movement.y = lerp(movement.normalized().y,0,1)
+		canJump = true
+		state_machine.travel("Idle")
 func runAnim():
 	state_machine.travel("Run")
 	
@@ -161,6 +182,7 @@ func projectile():
 		projectile.set_projectile_direction(-1)
 		
 	get_parent().add_child(projectile)
-	projectile.position = $WeaponControl/RangedWeaponPoint.global_position
-	
+	projectile.position = $WeaponControl/RangedWeaponPoint.global_position	
 	pass
+	
+
